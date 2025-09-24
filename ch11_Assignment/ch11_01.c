a04c55bb01bdb3ff8de84182b2ab5955906c6736
Kwangyeon11 <bnmliopa@gmail.com>
2025-09-24 19:10:03 +0900

    Update and rename chap11_1.c to ch11_01.c

--- ch11_Assignment/ch11_01.c
+++ ch11_Assignment/ch11_01.c
@@ -1,4 +1,8 @@
-// 25. 08. 01
+/*   파일명: ch11_01.c
+	 내  용: PA01. 정수를 매개변수로 전달해서 정수에 있는 숫자의 개수를 구하는 count_digits 함수를 재귀 함술 정의하시오.
+	 작성자: 주광연
+	 날  짜: 2025.09. 24
+*/
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
@@ -10,7 +14,7 @@ int count_digits(int n)
 {
 	if (n < 0) n = -n;
 	if (n < 10) return 1;
-	return 1 + count_digits(n / 10); // ���ǿ� ���� ���, return 1�� ������(�ڸ���)
+	return 1 + count_digits(n / 10); // 조건에 따라 재귀, return 1은 누적됨(자릿수)
 }
 
 void print_digit()
@@ -19,7 +23,7 @@ void print_digit()
 
 	while (1)
 	{
-		printf("����? ");
+		printf("정수? ");
 		scanf("%d", &num);
 
 		if (num == 0)
@@ -34,4 +38,5 @@ int main()
 {
 	print_digit();
 	return 0;
+
 }
