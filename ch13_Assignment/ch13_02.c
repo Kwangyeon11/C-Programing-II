742fe7b7d729c402df18a3339b95962ad1ce6264
Kwangyeon11 <bnmliopa@gmail.com>
2025-09-24 20:34:54 +0900

    Update and rename chap13_2.c to ch13_02.c

--- ch13_Assignment/ch13_02.c
+++ ch13_Assignment/ch13_02.c
@@ -1,3 +1,10 @@
+/*   파일명: ch13_02.c
+	 내  용: PA02. FILL_ARRAY 함수 매크로와 PRINT_ARRAY 함수 매크로를 정의하고, 
+     크기가 10인 int 배열에 대하여 입력받은 값으로 배열 전체를 채우고 출력하는 프로그램을 작성하시오.(난이도 1)
+	 작성자: 주광연
+	 날  짜: 2025.09. 24
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
@@ -19,7 +26,7 @@ void print_arr() {
     int x[10] = { 0 };
     const int size = 10;
 
-    printf("���� �迭�� �ʱⰪ? ");
+    printf("Á¤¼ö ¹è¿­ÀÇ ÃÊ±â°ª? ");
     scanf(" %d", &value);
 
     FILL_ARRAY(x, size, value);
@@ -32,3 +39,4 @@ int main() {
     print_arr();
     return 0;
 }
+
