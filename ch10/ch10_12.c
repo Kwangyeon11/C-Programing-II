d3f04855299350f94aa798c5549d615aede30caa
Kwangyeon11 <bnmliopa@gmail.com>
2025-10-01 11:55:51 +0900

    Update ch10_12.c

--- ch10/ch10_12.c
+++ ch10/ch10_12.c
@@ -1,5 +1,5 @@
 /*   파일명: ch10_12.c
-	 내  용: PA12. low word, high word 값을 매개변수로 전달받아 mydata 공용체로 만들어 리턴하는 make_dword 함수를 정의하시오.
+	 내  용: assignment. low word, high word 값을 매개변수로 전달받아 mydata 공용체로 만들어 리턴하는 make_dword 함수를 정의하시오.
 	 작성자: 주광연
 	 날  짜: 2025.10.01
 */
@@ -35,4 +35,5 @@ int main() {
 
     return 0;
 }
+
 
